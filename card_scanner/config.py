974fa723d32180e1fe46627816436f9655fd2908
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-08-20 08:40:36 +0300

    [RC-WS-T-20] Поправил конфигурацию сервисов

--- card_scanner/config.py
+++ card_scanner/config.py
@@ -1,6 +1,6 @@
 MODE_DEV = False
 
-CARD_URL = 'http://127.0.0.1:80/v1/slots/card'
+CARD_URL = 'http://127.0.0.1:80/v1/slots/set/card'
 HEADERS = {'Content-Type': 'application/json'}
 
 PORT = '/dev/serial0'
--- qr_code_scanner/config.py
+++ qr_code_scanner/config.py
@@ -1,7 +1,7 @@
 MODE_DEV = False
 
 HEADERS = {'Content-Type': 'application/json'}
-QR_URL = 'http://127.0.0.1:80/v1/slots/qr'
+QR_URL = 'http://127.0.0.1:80/v1/slots/set/qr'
 
 PORT = '/dev/input/event1'
 
