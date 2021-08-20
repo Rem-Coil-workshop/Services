31ef66b0e907e5159e6651d08b5e5df0784478da
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-08-20 15:26:55 +0300

    [RC-WS-T-29] Добавил обработку букв

--- qr_code_scanner/config.py
+++ qr_code_scanner/config.py
@@ -3,6 +3,6 @@ MODE_DEV = False
 HEADERS = {'Content-Type': 'application/json'}
 QR_URL = 'http://127.0.0.1:80/v1/slots/set/qr'
 
-PORT = '/dev/input/event1'
+PORT = '/dev/input/event0'
 
 SLEEP_TIME = 5
--- qr_code_scanner/hardware_manipulation.py
+++ qr_code_scanner/hardware_manipulation.py
@@ -3,6 +3,13 @@ import logging
 import evdev
 from config import PORT, MODE_DEV
 
+scancodes = {
+    2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8', 10: u'9', 11: u'0', 12: u'-', 16: u'Q',
+    17: u'W', 18: u'E', 19: u'R', 20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 30: u'A', 31: u'S',
+    32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 44: u'Z', 45: u'X', 46: u'C', 47: u'V',
+    48: u'B', 49: u'N', 50: u'M', 53: u'/', 54: u'RSHFT', 57: u' '
+}
+
 device = None
 
 
@@ -11,27 +18,28 @@ def set_up():
     device = evdev.InputDevice(PORT)
 
 
+# noinspection PyUnresolvedReferences
 def listen_changes() -> str:
     global device
     code = []
     data = ""
+    is_upper = True
 
     for event in device.read_loop():
         if event.type == evdev.ecodes.EV_KEY and event.value == 1:
-            e_code = event.code - 1
+            e_code = event.code
             if MODE_DEV:
                 logging.info('Считано значение: ' + str(e_code))
-            if 1 <= e_code <= 10:
-                digit = str(0)
-                if e_code != 10:
-                    digit = str(e_code)
-                code.append(digit)
-            elif e_code == 52:
-                code.append('/')
-            elif e_code == 11:
-                code.append('-')
-            elif e_code == 27:
+            if e_code == 28:
                 data = data.join(code)
                 break
+            elif e_code == 54:
+                is_upper = False
+            elif 16 <= e_code <= 50:
+                word = scancodes[e_code] if is_upper else scancodes[e_code].lower()
+                code.append(word)
+                is_upper = True
+            elif 2 <= e_code <= 11 or e_code == 53 or e_code == 12 or e_code == 57:
+                code.append(scancodes[e_code])
 
     return data
